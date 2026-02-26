03b13453f99b620104a65629184f374be5661ac3
Vitaliy Yurkin <68192985+Vit0607@users.noreply.github.com>
2026-02-26 15:44:19 +0300

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+
+@section('content')
+<h1>Hello about page</h1>
+@endsection
+
+
+@section('sidebar')
+HERE IS SIDEBAR
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+<h1>Hello main page</h1>
+@endsection
