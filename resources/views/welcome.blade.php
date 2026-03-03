299214a088aeb318d86e2fe5465d2c7b2475bac1
Vitaliy Yurkin <68192985+Vit0607@users.noreply.github.com>
2026-03-03 23:05:11 +0300

    upload image, display all images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,13 @@
 <div class="container">
     <h1 align="center">My Gallery</h1>
     <div class="row">
-        <div class="col-md-3 gallery-item"><img src="/image.png" class="img-thumbnail" alt="php">
+        @foreach ($imagesInView as $image)
+        <div class="col-md-3 gallery-item"><img src="/{{$image}}" class="img-thumbnail" alt="php">
             <a href="/show" class="btn btn-info my-button">Show</a>
             <a type="/edit" class="btn btn-warning my-button">Edit</a>
             <a type="#" class="btn btn-danger my-button">Delete</a>
         </div>
+        @endforeach
     </div>
 </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $images = DB::table('images')
+    ->select('*')
+    ->get();
+    $myImages = $images->pluck('image')->all();
+
+    return view('welcome', ['imagesInView' => $myImages]);
 });
 
 Route::get('/about', function () {
@@ -12,6 +19,17 @@ Route::get('/about', function () {
 
 Route::get('/create', function () {
     return view('create');
+});
+
+Route::post('/store', function (Request $request) {
+    $image = $request->file('image');
+    $filename = $request->image->store('uploads');
+
+    DB::table('images')->insert([
+        'image' => $filename
+    ]);
+
+    return redirect('/');
 });
 
 Route::get('/show', function () {
